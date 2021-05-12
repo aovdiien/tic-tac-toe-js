4de36f28eda664e61c0d9d9e072e4103782a68c1
Andrii Ovdiienko <andrii.ovdiienko@ibm.com>
2021-05-12 18:30:56 +0300

    configured eslint and prettier

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-"use strict";
+'use strict';
 
 window.addEventListener('load', app);
 
-let gameBoard = ['', '', '', '', '', '', '', '', '']; 
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let turn = 0; // Keeps track if X or O player's turn
 let winner = false;
 
 // CREATE PLAYER
-const player = (name) => {
-  name = name;
-  return {name};
- };
+const player = (name) => name;
 
- let playerX = player("");
- let playerY = player("");
+let playerX = player('');
+let playerY = player('');
 
- // INITIALIZE APP
+// INITIALIZE APP
 function app() {
-  let inputField = document.querySelector('.input-field').focus();
+  document.querySelector('.input-field').focus();
 
   const addPlayerForm = document.getElementById('player-form');
   addPlayerForm.addEventListener('submit', addPlayers);
@@ -51,12 +48,12 @@ function currentPlayer() {
 }
 
 // Resize squares in event browser is resized
-window.addEventListener("resize", onResize);
+window.addEventListener('resize', onResize);
 function onResize() {
   let allCells = document.querySelectorAll('.board__cell');
   let cellHeight = allCells[0].offsetWidth;
-  
-  allCells.forEach( cell => {
+
+  allCells.forEach((cell) => {
     cell.style.height = `${cellHeight}px`;
   });
 }
@@ -74,10 +71,10 @@ function buildBoard() {
 // CELL CLICK EVENT FOR PLAYER TO ATTEMPT TO MAKE MOVE
 function makeMove(event) {
   console.log(turn);
-  
+
   let currentCell = parseInt(event.currentTarget.firstElementChild.dataset.id);
   let cellToAddToken = document.querySelector(`[data-id='${currentCell}']`);
-  
+
   if (cellToAddToken.innerHTML !== '') {
     console.log('This cell is already taken.');
     return;
@@ -90,12 +87,12 @@ function makeMove(event) {
       gameBoard[currentCell] = 'O';
     }
   }
-    
+
   // CHECK IF WE HAVE A WINNER
   isWinner();
-    
+
   // Update turn count so next player can choose
-  turn ++;
+  turn++;
 
   // CHANGE BOARD HEADER INFO
   changeBoardHeaderNames();
@@ -103,7 +100,7 @@ function makeMove(event) {
 
 function checkIfTie() {
   if (turn > 7) {
-    alert('game over a tie')
+    alert('game over a tie');
   }
 }
 
@@ -116,10 +113,10 @@ function isWinner() {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
 
-  winningSequences.forEach( winningCombos => {
+  winningSequences.forEach((winningCombos) => {
     let cell1 = winningCombos[0];
     let cell2 = winningCombos[1];
     let cell3 = winningCombos[2];
@@ -128,17 +125,12 @@ function isWinner() {
       gameBoard[cell2] === currentPlayer() &&
       gameBoard[cell3] === currentPlayer()
     ) {
-
-      
       const cells = document.querySelectorAll('.board__cell');
-      let letterId1 = document.querySelector(`[data-id='${cell1}']`);
-      let letterId2 = document.querySelector(`[data-id='${cell2}']`);
-      let letterId3 = document.querySelector(`[data-id='${cell3}']`);
-      
-      cells.forEach( cell => {
-        let cellId = cell.firstElementChild.dataset.id;	
 
-        if (cellId == cell1 || cellId == cell2 || cellId == cell3 ) {
+      cells.forEach((cell) => {
+        let cellId = cell.firstElementChild.dataset.id;
+
+        if (cellId == cell1 || cellId == cell2 || cellId == cell3) {
           cell.classList.add('board__cell--winner');
         }
       });
@@ -167,7 +159,7 @@ function isWinner() {
   if (!winner) {
     checkIfTie();
   }
-  
+
   return false;
 }
 
@@ -178,23 +170,23 @@ function changeBoardHeaderNames() {
       currentPlayerText.innerHTML = `
         <span class="name--style">${playerX.name}</span>, you are up!
         <div class="u-r-winner"></div>
-      `
-    }  else {
+      `;
+    } else {
       currentPlayerText.innerHTML = `
         <span class="name--style">${playerY.name}</span>, you are up.
         <div class="u-r-winner"></div>
-      `
+      `;
     }
   }
 }
 
 function resetBoard() {
   console.log('resetting');
-  
-  gameBoard = ['', '', '', '', '', '', '', '', '']; 
-  
+
+  gameBoard = ['', '', '', '', '', '', '', '', ''];
+
   let cellToAddToken = document.querySelectorAll('.letter');
-  cellToAddToken.forEach( square => {
+  cellToAddToken.forEach((square) => {
     square.textContent = '';
     square.parentElement.classList.remove('board__cell--winner');
   });
@@ -206,22 +198,21 @@ function resetBoard() {
   currentPlayerText.innerHTML = `
     <span class="name--style">${playerX.name}</span>, you are up!
     <div class="u-r-winner"></div>
-  `
+  `;
 
   addCellClickListener();
 }
 
 function addCellClickListener() {
   const cells = document.querySelectorAll('.board__cell');
-  cells.forEach( cell => {
+  cells.forEach((cell) => {
     cell.addEventListener('click', makeMove);
   });
 }
 
 function removeCellClickListener() {
   let allCells = document.querySelectorAll('.board__cell');
-  allCells.forEach( cell => {
+  allCells.forEach((cell) => {
     cell.removeEventListener('click', makeMove);
   });
 }
-
